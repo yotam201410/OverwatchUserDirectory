2e077ac4ff266fd30c76e953edf881400228531d
yotam <yotam201410@gmail.com>
2020-06-05 22:43:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="OverwatchUserDirectory",
-    version="1.0",
+    version="1.1",
     author="Yotam",
     author_email="yotam201410@gmail.com",
     description="An Overwatch user directory that gets everything you can take from Overwatch API using ovrstat(https://ovrstat.com) ",
