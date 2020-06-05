1eca4b44323ca44761f7c8e5418819e4917653bf
yotam <yotam201410@gmail.com>
2020-06-05 21:42:24 +0300

    s

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="OverwatchUserDirectory", # Replace with your own username
+    name="Overwatch-User-API",
     version="1.0",
     author="Yotam",
     author_email="yotam201410@gmail.com",
     description="An Overwatch user directory that gets everything you can take from overwatch API using ovrstat(https://ovrstat.com) ",
     url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
+    lisence="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3',
 )
