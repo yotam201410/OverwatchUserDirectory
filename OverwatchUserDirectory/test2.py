0eb18da5a4c31579acde7704c7cb60957e2118f0
yotam <yotam201410@gmail.com>
2020-06-05 21:44:11 +0300

    s

--- OverwatchUserDirectory/test2.py
+++
@@ -1,3 +0,0 @@
-from OverwatchUserDirectory.User import User
-user = User('yotam201410#2946')
-print(user.quickPlayStats.career_stats.hero_dict["sigma"].matchAwards)
--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Overwatch-User-API",
+    name="OverwatchUserDirectory",
     version="1.0",
     author="Yotam",
     author_email="yotam201410@gmail.com",
-    description="An Overwatch user directory that gets everything you can take from overwatch API using ovrstat(https://ovrstat.com) ",
-    url="https://github.com/pypa/sampleproject",
+    description="An Overwatch user directory that gets everything you can take from Overwatch API using ovrstat(https://ovrstat.com) ",
+    url="https://github.com/yotam201410/OverwatchUserDirectory",
     packages=setuptools.find_packages(),
     lisence="MIT",
     classifiers=[
